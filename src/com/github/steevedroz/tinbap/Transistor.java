41a2b0c28d471408c001343bc53a50ae80872a01
Steeve Droz <steeve.droz@sudest-webdesign.com>
2016-10-24 07:40:44 +0200

    Added threshold

--- src/com/github/steevedroz/tinbap/Transistor.java
+++ src/com/github/steevedroz/tinbap/Transistor.java
@@ -2,24 +2,39 @@ package com.github.steevedroz.tinbap;
 
 public class Transistor extends Component {
     private static final String NAME = "Transistor";
+    private static final double THRESHOLD = 0.2;
 
     private Connector base;
     private Connector collector;
     private Connector emitter;
+    private double threshold;
 
     public Transistor() {
+	this(THRESHOLD);
+    }
+
+    public Transistor(double threshold) {
 	super(NAME);
 	this.base = new Connector("Base", IOType.IN);
 	this.collector = new Connector("Collector", IOType.BOTH);
 	this.emitter = new Connector("Emitter", IOType.BOTH);
+	this.threshold = threshold;
     }
 
     @Override
     public void eval() {
-	if (base.getVoltage() > 0.2) {
+	if (base.getVoltage() > threshold) {
 	    double maxVoltage = Math.max(collector.getVoltage(), emitter.getVoltage());
 	    collector.setVoltage(maxVoltage);
 	    emitter.setVoltage(maxVoltage);
 	}
+    }
+
+    public double getThreshold() {
+	return threshold;
+    }
+
+    public void setThreshold(double threshold) {
+	this.threshold = threshold;
     }
 }
